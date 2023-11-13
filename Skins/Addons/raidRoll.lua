714ec18bd79a45b889ec62b5b92fb8e3aa2b8c22
fxpw <fxpw@outlook.com>
2023-11-13 20:57:11 +0300

    fix rr

--- Skins/Addons/raidRoll.lua
+++ Skins/Addons/raidRoll.lua
@@ -27,25 +27,37 @@ S:AddCallbackForAddon("RaidRoll", "RaidRoll", function()
 	S:HandleButton(RR_Clear)
 	S:HandleButton(RR_Next)
 	S:HandleButton(RaidRoll_OptionButton)
-
-	RR_Roll_5SecAndAnnounce:ClearAllPoints()
-	RR_Roll_5SecAndAnnounce:Point("BOTTOM", 0, 31)
-
-	RR_Clear:Point("BOTTOM", 0, 8)
-	RR_Last:Point("BOTTOM", -45, 8)
-	RR_Roll_RollButton:Point("BOTTOMRIGHT", RR_RollFrame, "BOTTOM", -65, 8)
-	RR_Next:Point("BOTTOM", 45, 8)
-	RaidRoll_OptionButton:Size(20)
-	RaidRoll_OptionButton:Point("BOTTOM", 75, 8)
-
-	RR_Frame:SetTemplate("Transparent")
-	RR_Frame:Width(185)
-	RR_Frame:Point("TOP", RR_RollFrame, "BOTTOM", 0, 1)
-
-	local rrframeLevel = RR_Frame:GetFrameLevel()
-	RaidRoll_Catch_All:SetFrameLevel(rrframeLevel + 2)
-	RaidRoll_Allow_All:SetFrameLevel(rrframeLevel + 2)
-	RaidRollCheckBox_ExtraRolls:SetFrameLevel(rrframeLevel + 2)
+	if RR_Roll_5SecAndAnnounce then
+		RR_Roll_5SecAndAnnounce:ClearAllPoints()
+		RR_Roll_5SecAndAnnounce:Point("BOTTOM", 0, 31)
+	end
+	if RR_Clear then
+		RR_Clear:Point("BOTTOM", 0, 8)
+	end
+	if RR_Last then
+		RR_Last:Point("BOTTOM", -45, 8)
+	end
+	if RR_Roll_RollButton then
+		RR_Roll_RollButton:Point("BOTTOMRIGHT", RR_RollFrame, "BOTTOM", -65, 8)
+	end
+	if RR_Next then
+		RR_Next:Point("BOTTOM", 45, 8)
+	end
+	if RaidRoll_OptionButton then
+		RaidRoll_OptionButton:Size(20)
+		RaidRoll_OptionButton:Point("BOTTOM", 75, 8)
+	end
+	if RR_Frame then
+		RR_Frame:SetTemplate("Transparent")
+		RR_Frame:Width(185)
+		RR_Frame:Point("TOP", RR_RollFrame, "BOTTOM", 0, 1)
+	end
+	if RR_Frame and  RaidRoll_Catch_All and RaidRoll_Allow_All and RaidRollCheckBox_ExtraRolls then
+		local rrframeLevel = RR_Frame:GetFrameLevel()
+		RaidRoll_Catch_All:SetFrameLevel(rrframeLevel + 2)
+		RaidRoll_Allow_All:SetFrameLevel(rrframeLevel + 2)
+		RaidRollCheckBox_ExtraRolls:SetFrameLevel(rrframeLevel + 2)
+	end
 	S:HandleCheckBox(RaidRoll_Catch_All)
 	S:HandleCheckBox(RaidRoll_Allow_All)
 	S:HandleCheckBox(RaidRollCheckBox_ExtraRolls)
@@ -56,13 +68,15 @@ S:AddCallbackForAddon("RaidRoll", "RaidRoll", function()
 
 	for i = 1, 5 do
 		local f = _G["Raid_Roll_SetSymbol"..i]
-		f:ClearAllPoints()
-		f:Point("TOPLEFT", _G["RR_RollerPos"..i], "TOPRIGHT", -15, -1)
-		f:Point("BOTTOMRIGHT", _G["RR_Rolled"..i], "BOTTOMLEFT", 45, -1)
+		if f then
+			f:ClearAllPoints()
+			f:Point("TOPLEFT", _G["RR_RollerPos"..i], "TOPRIGHT", -15, -1)
+			f:Point("BOTTOMRIGHT", _G["RR_Rolled"..i], "BOTTOMLEFT", 45, -1)
 
-		local highlight = f:GetHighlightTexture()
-		highlight:SetTexture(E.Media.Textures.Highlight)
-		highlight:SetVertexColor(0.9, 0.9, 0.9, 0.35)
+			local highlight = f:GetHighlightTexture()
+			highlight:SetTexture(E.Media.Textures.Highlight)
+			highlight:SetVertexColor(0.9, 0.9, 0.9, 0.35)
+		end
 	end
 
 	if E.private.general.replaceBlizzFonts and GetLocale() ~= "zhCN" then
@@ -74,15 +88,18 @@ S:AddCallbackForAddon("RaidRoll", "RaidRoll", function()
 		end
 
 		for i = 1, 5 do
-			_G["RR_Roller"..i].SetFont = updateFont
+			if _G["RR_Roller"..i] then
+				_G["RR_Roller"..i].SetFont = updateFont
+			end
 		end
 	end
 end)
 
 S:AddCallbackForAddon("RaidRoll_LootTracker", "RaidRoll_LootTracker", function()
 	if not E.private.addOnSkins.RaidRoll then return end
-
-	RR_LOOT_FRAME:SetTemplate("Transparent")
+	if RR_LOOT_FRAME then
+		RR_LOOT_FRAME:SetTemplate("Transparent")
+	end
 
 	S:HandleSliderFrame(RaidRoll_Loot_Slider_ID)
 
@@ -94,22 +111,30 @@ S:AddCallbackForAddon("RaidRoll_LootTracker", "RaidRoll_LootTracker", function()
 	S:HandleButton(RR_Loot_ButtonLast)
 
 	for i = 1, 4 do
-		_G["RR_Loot_Announce_1_Button_"..i]:Show()
-		_G["RR_Loot_Announce_2_Button_"..i]:Show()
-		_G["RR_Loot_Announce_3_Button_"..i]:Show()
-		_G["RR_Loot_RaidRollButton_"..i]:Show()
-
+		if _G["RR_Loot_Announce_1_Button_"..i] then
+			_G["RR_Loot_Announce_1_Button_"..i]:Show()
+		end
+		if _G["RR_Loot_Announce_2_Button_"..i] then
+			_G["RR_Loot_Announce_2_Button_"..i]:Show()
+		end
+		if _G["RR_Loot_Announce_3_Button_"..i] then
+			_G["RR_Loot_Announce_3_Button_"..i]:Show()
+		end
+		if _G["RR_Loot_RaidRollButton_"..i] then
+			_G["RR_Loot_RaidRollButton_"..i]:Show()
+		end
 		S:HandleButton(_G["RR_Loot_Announce_1_Button_"..i])
 		S:HandleButton(_G["RR_Loot_Announce_2_Button_"..i])
 		S:HandleButton(_G["RR_Loot_Announce_3_Button_"..i])
 		S:HandleButton(_G["RR_Loot_RaidRollButton_"..i])
 	end
-
-	for i = 1, RR_LOOT_FRAME:GetNumChildren() do
-		local child = select(i, RR_LOOT_FRAME:GetChildren())
-		if child and child:IsObjectType("Button") and child:GetName() == "Close_Button" then
-			S:HandleCloseButton(child)
-			break
+	if RR_LOOT_FRAME then
+		for i = 1, RR_LOOT_FRAME:GetNumChildren() do
+			local child = select(i, RR_LOOT_FRAME:GetChildren())
+			if child and child:IsObjectType("Button") and child:GetName() == "Close_Button" then
+				S:HandleCloseButton(child)
+				break
+			end
 		end
 	end
 end)
