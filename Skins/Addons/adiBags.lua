e5f83c523236492b91e93c6e48b47326a21083a3
Accidev <accidev@yandex.ru>
2026-02-21 20:25:26 +0300

    fix skin AdiBags

--- Skins/Addons/adiBags.lua
+++ Skins/Addons/adiBags.lua
@@ -17,11 +17,28 @@ local ITEM_QUALITY_UNCOMMON = ITEM_QUALITY_UNCOMMON
 local TEXTURE_ITEM_QUEST_BANG = TEXTURE_ITEM_QUEST_BANG
 local TEXTURE_ITEM_QUEST_BORDER = TEXTURE_ITEM_QUEST_BORDER
 
--- AdiBags 1.1 beta 7
--- https://www.curseforge.com/wow/addons/adibags/files/452440
+-- AdiBags by Accidev
+-- https://github.com/accidev/AdiBags-for-Sirus
 
 S:AddCallbackForAddon("AdiBags", "AdiBags", function()
 	if not E.private.addOnSkins.AdiBags then return end
+
+	if E.private.bags.enable then
+		E.PopupDialogs.ADIBAGS_ELVUI_BAGS = {
+			text = "У вас включен встроенный модуль Сумок |cff1784d1ElvUI|r, и при этом работает аддон |cff00bfffAdiBags|r. Это вызывает двойное открытие банков и инвентаря. Желаете отключить встроенный модуль сумок ElvUI?",
+			button1 = ACCEPT,
+			button2 = CANCEL,
+			OnAccept = function()
+				E.private.bags.enable = false
+				ReloadUI()
+			end,
+			timeout = 0,
+			whileDead = 1,
+			hideOnEscape = false,
+		}
+		E:StaticPopup_Show("ADIBAGS_ELVUI_BAGS")
+	end
+
 
 	local AdiBags = LibStub("AceAddon-3.0"):GetAddon("AdiBags", true)
 	if not AdiBags then return end
@@ -73,28 +90,32 @@ S:AddCallbackForAddon("AdiBags", "AdiBags", function()
 	end
 
 	local LayeredRegionClass = AdiBags:GetClass("LayeredRegion")
-	hooksecurefunc(LayeredRegionClass.prototype, "AddWidget", function(self, widget)
-		if widget:IsObjectType("Button") then
-			if widget:GetText() then
-				S:HandleButton(widget)
-			else
-				widget:StyleButton(true, true)
-				-- local nt = widget:GetNormalTexture()
-				if widget:GetNormalTexture() then
-					widget:GetNormalTexture():SetTexCoord(unpack(E.TexCoords))
+	if LayeredRegionClass then
+		hooksecurefunc(LayeredRegionClass.prototype, "AddWidget", function(self, widget)
+			if widget:IsObjectType("Button") then
+				if widget:GetText() then
+					S:HandleButton(widget)
+				else
+					widget:StyleButton(true, true)
+					if widget:GetNormalTexture() then
+						widget:GetNormalTexture():SetTexCoord(unpack(E.TexCoords))
+					end
+					if widget:GetPushedTexture() then
+						widget:GetPushedTexture():SetTexCoord(unpack(E.TexCoords))
+					end
 				end
-				-- local ct = widget:GetCheckedTexture()
-				if widget:GetNormalTexture() then
-					widget:GetNormalTexture():SetTexCoord(unpack(E.TexCoords))
+			elseif widget:IsObjectType("EditBox") then
+				widget:DisableDrawLayer("BACKGROUND")
+				S:HandleEditBox(widget)
+
+				if widget.clearButton then
+					widget.clearButton:StripTextures()
+					widget.clearButton:SetText("")
+					S:HandleCloseButton(widget.clearButton)
 				end
 			end
-		elseif widget.editBox and widget.editBox.clearButton then
-			widget.editBox:DisableDrawLayer("BACKGROUND")
-			S:HandleEditBox(widget.editBox)
-
-			S:HandleButton(widget.editBox.clearButton)
-		end
-	end)
+		end)
+	end
 
 	local function updateBorderTexture(self, texture, g, b)
 		if texture == TEXTURE_ITEM_QUEST_BANG then
@@ -165,6 +186,44 @@ S:AddCallbackForAddon("AdiBags", "AdiBags", function()
 				self.parent:SetBackdropBorderColor(color, color, color, 0.2)
 			end
 		end
+	end
+
+	local SectionClass = AdiBags:GetClass("Section")
+	if SectionClass then
+		hooksecurefunc(SectionClass.prototype, "OnCreate", function(self)
+			local header = self.Header
+			if header:GetHighlightTexture() then
+				header:GetHighlightTexture():SetTexture(nil)
+			else
+				header:SetHighlightTexture("")
+			end
+
+			local r, g, b = unpack(E.media.rgbvaluecolor)
+
+			local left = header:CreateTexture(nil, "HIGHLIGHT")
+			left:SetTexture(r, g, b, 1)
+			left:SetPoint("TOPLEFT")
+			left:SetPoint("BOTTOMLEFT")
+			left:SetWidth(1)
+
+			local right = header:CreateTexture(nil, "HIGHLIGHT")
+			right:SetTexture(r, g, b, 1)
+			right:SetPoint("TOPRIGHT")
+			right:SetPoint("BOTTOMRIGHT")
+			right:SetWidth(1)
+
+			local top = header:CreateTexture(nil, "HIGHLIGHT")
+			top:SetTexture(r, g, b, 1)
+			top:SetPoint("TOPLEFT")
+			top:SetPoint("TOPRIGHT")
+			top:SetHeight(1)
+
+			local bottom = header:CreateTexture(nil, "HIGHLIGHT")
+			bottom:SetTexture(r, g, b, 1)
+			bottom:SetPoint("BOTTOMLEFT")
+			bottom:SetPoint("BOTTOMRIGHT")
+			bottom:SetHeight(1)
+		end)
 	end
 
 	local ItemButtonClass = AdiBags:GetClass("ItemButton")
